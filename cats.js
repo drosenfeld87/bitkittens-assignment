4bc817bdab6ac18c6ccd66fe4740b9d932de718e
debbie <drosenfeld87@gmail.com>
2018-03-27 16:08:06 -0400

    cats are added to room

--- cats.js
+++ cats.js
@@ -1,15 +1,22 @@
       document.addEventListener("DOMContentLoaded", function() {
 
-        var button = document.querySelector('.summon-cats')
-        button.addEventListener('click', function() {
+      var button = document.querySelector('.summon-cats')
+      button.addEventListener('click', function() {
 
       var request = $.ajax ({
       url: 'http://bitkittens.herokuapp.com/cats.json',
       method: 'GET',
       data: {},
       dataType: 'json'
-      }).done(function () {
-     console.log("request succeeded");
-      });
-    });
+    }).done(function (responseData) {
+        var divCats = document.getElementsByClassName("cat-box");
+
+        for(var i = 0; i < responseData.cats.length; i++) {
+          var img = document.createElement('img');
+
+			  img.src = responseData.cats[i].photo;
+        divCats[i].appendChild(img);
+    };
+  });
+  });
   });
